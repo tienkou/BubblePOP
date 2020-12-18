58315d1e5d4c812b08bed46710fe93d88a5c0d60
Harja Jonne <tienkou@gmail.com>
2020-12-18 17:43:28 +0200

    Score lähes valmis. Populate toimii.

--- BubblePOP/BubblePOP.cs
+++ BubblePOP/BubblePOP.cs
@@ -5,19 +5,24 @@ using Jypeli.Assets;
 using Jypeli.Controls;
 using Jypeli.Widgets;
 
+/// <summary>
+/// @author Harja "tienkou" Jonne
+/// @version 1.0 - gold
+/// </summary>
 public class BubblePOP : PhysicsGame
 {
     
 
-    private int BUBBLES = 5;
-    public int pallojaCount = 0;
+    private int BUBBLES = 75;
+    private int pallojaCount = 0;
 
     private Image mapImage = LoadImage("map");  
     private Shape mapShape;
 
     private PhysicsObject Bubble;
 
-    List<PhysicsObject> balls = new List<PhysicsObject> ();
+    List<PhysicsObject> bubbles = new List<PhysicsObject> ();
+    List<Color> bubbleColors = new List<Color> {Color.Yellow, Color.White, Color.Blue, Color.Green, Color.HotPink };
 
     IntMeter scoreCounter;
     Timer timer = new Timer();
@@ -47,8 +52,22 @@ public class BubblePOP : PhysicsGame
     private void BubblePop(PhysicsObject bubble)
     {
         bubble.Destroy();
-        balls.Remove(bubble);
-        //if (bubble.Color) BubblePOP(this);
+        bubbles.Remove(bubble);
+        double distance = 0.0;
+
+
+
+        for (int i = 0; i < bubbles.Count; i++)
+        {
+            distance = Vector.Distance(bubble.Position, bubbles[i].Position);
+            if (bubble.Color == bubbles[i].Color && distance < 60) 
+            {
+                BubblePop(bubbles[i]);
+                i--;
+            }
+        }
+        
+
         scoreCounter.Value += 1;
         scoreCounter.UpperLimit += KaikkiKeratty;
     }
@@ -60,37 +79,36 @@ public class BubblePOP : PhysicsGame
         Add(map);
     }
 
-    void Populate()
+    private void Populate()
     {
         Vector position = new Vector(0, 400);
 
-        Timer.CreateAndStart(0.2, delegate { CreateBubble(position); }
-            );
-        if (pallojaCount < BUBBLES) timer.Stop();
-        /*  
         for (int i = 0; i < BUBBLES; i++)
           {
-            timer.Stop();  
-            //Timer.SingleShot(0.2,
-                 // delegate { CreateBubble(position); }
-                 // );
+            //timer.Stop();  
+            Timer.SingleShot(0.1+0.1*i,
+                 delegate { CreateBubble(position); }
+                 );
           }
-        */
+       
 
     }
-
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="position"></param>
     private void CreateBubble(Vector position)
     {
         
         Bubble = new PhysicsObject(50, 50);
         Bubble.Shape = Shape.Circle;
-        Bubble.Color = RandomGen.NextColor(); 
+        Bubble.Color = RandomGen.SelectOne<Color>(bubbleColors); 
         Bubble.Position = position;
         Bubble.Mass = 15.0;
         Bubble.Restitution = 0.1;
         pallojaCount++;
         Add(Bubble);
-        balls.Add(Bubble);
+        bubbles.Add(Bubble);
         Mouse.ListenOn(Bubble, MouseButton.Left, ButtonState.Pressed, BubblePop, null, Bubble);
         
 
@@ -98,13 +116,14 @@ public class BubblePOP : PhysicsGame
         // Softness voi olla olennainen game feeling kannalta? https://trac.cc.jyu.fi/projects/npo/wiki/OliotJaSelitykset#Tärkeimmätominaisuudet5
     }
 
-    void CreateScoreCounter ()
+    private void CreateScoreCounter ()
     {
+        int tempScore = BUBBLES;
         scoreCounter = new IntMeter(0);
-        scoreCounter.MaxValue = 1;
+        scoreCounter.MaxValue = tempScore - (int)(tempScore * 0.75);
 
         Label scoreLabel = new Label();
-        scoreLabel.Y  =+ 300;
+        scoreLabel.Y += 300;
         scoreLabel.TextColor = Color.Yellow;
         scoreLabel.Color = Color.Black;
 
@@ -113,10 +132,12 @@ public class BubblePOP : PhysicsGame
     }
 
 
-    void KaikkiKeratty()
+    //#TODO: Funktio jolla on attribuutteja ja palauttaa sen
+    //scoreCounter.MaxValue = tempScore - (int) (tempScore* 0.75);
+
+    private void KaikkiKeratty()
     {
         MessageDisplay.Add("Pelaaja 1 voitti pelin.");
     }
 
 }
-
